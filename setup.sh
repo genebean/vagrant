3529f7e88934c6b5528f33cf6979b625e91fc3a1
ack81 <akinney@clousboltsoftware.com>
2016-06-07 10:35:25 -0400

    remove submodule update

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 echo "Cloning CloudBolt Demo (Vagrant)..."
 git clone https://github.com/ack81/cbdemo.git ~/cbdemo
 cd ~/cbdemo
-git submodule --quiet update --init
+#git submodule --quiet update --init
 ./cbdemo.sh
